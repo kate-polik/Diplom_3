7b1c39c3fe2c1c615d374f9025dbb24214b56db2
Ekaterina Polikarpova <katya.polikarpovaa@gmail.com>
2025-03-07 06:50:07 +0300

    test(docs): Мелкие правки про проекту

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -10,13 +10,11 @@ class MainPageLocators:
     SECOND_INGREDIENT = (By.XPATH,
                          "//p[@class='BurgerIngredient_ingredient__text__yp3dH'][contains(text(),'Краторная булка N-200i')]")
 
-
-
-
     INGREDIENT_COUNTER = (
         By.XPATH, "//p[@class='counter_counter__num__3nue1'][contains(text(),'2')]")  # Счетчик ингредиента
 
     ORDER_CONSTRUCTOR = (By.XPATH, "//div[@class='constructor-element constructor-element_pos_top']")
     ORDER_BUTTON = (By.XPATH, "//button[contains(text(),'Оформить заказ')]")
     CLOSE_MODAL_BUTTON = (By.XPATH, "//button[@type='button']")
-    ORDER_NUMBER = (By.XPATH, "//h2[@class='Modal_modal__title_shadow__3ikwq Modal_modal__title__2L34m text text_type_digits-large mb-8']")
+    ORDER_NUMBER = (By.XPATH,
+                    "//h2[@class='Modal_modal__title_shadow__3ikwq Modal_modal__title__2L34m text text_type_digits-large mb-8']")
